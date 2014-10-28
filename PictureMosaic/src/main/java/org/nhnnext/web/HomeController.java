99e30f13c65306ac8f3cd3f0554ee783d4ef758b
skyhills13 <skyhills13@gmail.com>
2014-10-28 17:11:06 +0900

    fix the compile error and change welcomefilelists

--- PictureMosaic/src/main/java/org/nhnnext/web/HomeController.java
+++ PictureMosaic/src/main/java/org/nhnnext/web/HomeController.java
@@ -1,13 +1,18 @@
 package org.nhnnext.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping("/")
 	public String home(){
+		System.out.println("aaa");
+		logger.debug("at home");
 		return "index";
 	}
 
