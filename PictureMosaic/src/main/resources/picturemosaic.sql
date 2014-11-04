938a25a0f58da3c3561b83d9d6cd2439c981fab1
skyhills13 <skyhills13@gmail.com>
2014-11-04 11:45:51 +0900

    #93 reverse the sql file to handle error

--- PictureMosaic/src/main/resources/picturemosaic.sql
+++ PictureMosaic/src/main/resources/picturemosaic.sql
@@ -4,11 +4,6 @@ CREATE TABLE PHOTOS (
  id int NOT NULL auto_increment, 
  unique_id varchar(60) NOT NULL,
  original_name varchar(45) NOT NULL, 
- mosaic_id int NOT NULL,
  primary key(id) 
 );
 
-CREATE TABLE MOSAICS (
- id int NOT NULL AUTO_INCREMENT,
- total_number int NOT NULL
-);
