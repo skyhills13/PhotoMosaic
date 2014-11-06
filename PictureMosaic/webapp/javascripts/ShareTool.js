3d0e9e7d8faa9d54c7837380e4148d47c288b5ed
kimjoohwee <kjhwee91@gmail.com>
2014-11-06 10:32:10 +0900

    #87 개발중

---
+++ PictureMosaic/webapp/javascripts/ShareTool.js
@@ -0,0 +1,23 @@
+
+
+function ShareTool(){
+}
+
+
+ShareUrl.prototype = {
+	URL : function(sButton){
+		var currentUrl = this.getCurrentUrl();
+		this.copyURL(sButton, currentUrl);
+	},
+	
+	getCurrentUrl : function(){
+		return location.href;
+	},
+	
+	copyURL : function(button, url){
+		button.addEventListener("click", function(){
+			url.execCommand('copy');
+			alert(url + "복사되었습니다.");
+		});
+	}
+}
