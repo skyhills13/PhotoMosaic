bd062537a5f2bf2b518f6ddeca8ca6a7cd1fdce9
LEE-GyeongMin <min20@nhnnext.org>
2014-11-11 10:09:18 +0900

    UI Test를 위한 URL Mapping 추가

--- PhotoMosaic/src/main/java/org/nhnnext/web/HomeController.java
+++ PhotoMosaic/src/main/java/org/nhnnext/web/HomeController.java
@@ -14,5 +14,12 @@ public class HomeController {
 		logger.debug("at home, which is select page");
 		return "select";
 	}
+	
+	// UI Test를 위한 URL Mapping. file 이름만 바꾸면 된다.
+	// TODO 배포 전에 반드시 제거할 것.
+	@RequestMapping("/test")
+	public String UITest() {
+		return "targetFile";
+	}
 
 }
