8b3c287e244ac7d1345c25b6f291f73ff1f9cd90
LEE-GyeongMin <min20@nhnnext.org>
2014-11-13 01:50:38 +0900

    UI Test용 URL Mapping 추가, delete commit

--- PhotoMosaic/src/main/java/org/nhnnext/web/HomeController.java
+++ PhotoMosaic/src/main/java/org/nhnnext/web/HomeController.java
@@ -3,6 +3,7 @@ package org.nhnnext.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,9 +18,9 @@ public class HomeController {
 	
 	// UI Test를 위한 URL Mapping. file 이름만 바꾸면 된다.
 	// TODO 배포 전에 반드시 제거할 것.
-	@RequestMapping("/test")
-	public String UITest() {
-		return "combineTest";
+	@RequestMapping("/test/{testPage}")
+	public String UITest(@PathVariable String testPage) {
+		return "/test/" + testPage;
 	}
 
 }
