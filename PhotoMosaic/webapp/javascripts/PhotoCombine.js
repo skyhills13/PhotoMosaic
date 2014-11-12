a15fd166aba740e4f44a465f53e82964e887bf7f
kimjoohwee <kjhwee91@gmail.com>
2014-11-12 22:05:22 +0900

    combine 하는 과정에서 이미지가 보여질 위치 정하는 함수 작업중 (adjustImagePosition)

--- PhotoMosaic/webapp/javascripts/PhotoCombine.js
+++ PhotoMosaic/webapp/javascripts/PhotoCombine.js
@@ -1,13 +1,26 @@
 var ooo;
 window.addEventListener("load", function() {
-	var tg = new TemplateGenerator();
-	tg.saveTargetTemplates(8);
-	var random = parseInt(Math.random()*1000);
-	var tArray = tg.targetTemplates[random].template;
-	
+	// var tg = new TemplateGenerator();
+	// tg.saveTargetTemplates(8);
+	// var random = parseInt(Math.random()*1000);
+	// var tArray = tg.targetTemplates[random].template;
+
+	var t = [ [ "1x1", "1x1", "1x4", "1x4", "1x1", "1x1", "x", "x", "2x1", "x", "x", "x", "2x1", "x", "x", "x" ],
+			[ "1x1", "2x1", "x", "1x1", "1x1", "2x2", "x", "1x2", "1x1", "x", "x", "x", "4x1", "x", "x", "x" ],
+			[ "1x1", "1x1", "1x1", "1x1", "2x2", "x", "2x2", "x", "x", "x", "x", "x", "1x1", "3x1", "x", "x" ],
+			[ "1x1", "1x1", "1x1", "1x1", "4x2", "x", "x", "x", "x", "x", "x", "x", "1x1", "1x1", "2x1", "x" ],
+			[ "1x1", "1x1", "1x2", "1x1", "2x1", "x", "x", "1x2", "3x2", "x", "x", "x", "x", "x", "x", "1x1" ],
+			[ "1x1", "1x1", "1x2", "1x1", "2x1", "x", "x", "1x2", "3x2", "x", "x", "x", "x", "x", "x", "1x1" ],
+			[ "1x1", "2x1", "x", "1x1", "1x1", "2x2", "x", "1x2", "1x1", "x", "x", "x", "4x1", "x", "x", "x" ],
+			[ "1x1", "1x1", "1x1", "1x1", "2x2", "x", "2x2", "x", "x", "x", "x", "x", "1x1", "3x1", "x", "x" ],
+			[ "1x1", "1x1", "1x1", "1x1", "4x2", "x", "x", "x", "x", "x", "x", "x", "1x1", "1x1", "2x1", "x" ],
+			[ "1x1", "1x1", "1x2", "1x1", "2x1", "x", "x", "1x2", "3x2", "x", "x", "x", "x", "x", "x", "1x1" ] ];
+
+	var tArray = t[parseInt(Math.random() * 10)];
+
 	var pArray = document.querySelectorAll("#list img");
 	var appendPlace = document.querySelector("#canvas");
-	
+
 	var combine = new PhotoCombine();
 	combine.getMaterial({
 		"mWidth" : 1000,
@@ -20,13 +33,12 @@ window.addEventListener("load", function() {
 	});
 	combine.create();
 	var result = combine.getResult();
-	document.querySelector("p a").addEventListener("click", function(){
+	document.querySelector("p a").addEventListener("click", function() {
 		window.location = result;
 	});
 });
 
 function PhotoCombine() {
-
 }
 
 PhotoCombine.prototype = {
@@ -49,15 +61,22 @@ PhotoCombine.prototype = {
 			this.drawingTargetImage(item);
 		}.bind(this));
 	},
-	
-	getResult : function(){
+
+	getResult : function() {
 		return this.mainCanvas.toDataURL();
 	},
-	drawingTargetImage : function(i) {
+	
+	drawingTargetImage : function(item) {
+		// var i = this.adjustImagePosition(item);
+		var i = item;
 		var img = this.photoArray[0];
 		var canvas = this.mainCanvas;
 		var context = canvas.getContext("2d");
 		context.drawImage(i.imgElement, 0, 0, i._width, i._height, i.posX, i.posY, i._width, i._height);
+	},	
+	
+	adjustImagePosition : function(item){
+		return item;
 	},
 
 	linkBoardWithImage : function() {
