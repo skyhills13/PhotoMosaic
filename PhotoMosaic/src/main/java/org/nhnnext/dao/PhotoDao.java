bbb78bc944eefcf507f6bcc7505964dc3806f2a0
skyhills13 <skyhills13@gmail.com>
2014-11-12 16:19:25 +0900

    revert project not to use mybatis

--- PhotoMosaic/src/main/java/org/nhnnext/dao/PhotoDao.java
+++ PhotoMosaic/src/main/java/org/nhnnext/dao/PhotoDao.java
@@ -1,19 +1,75 @@
 package org.nhnnext.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+
 import org.nhnnext.domains.Photo;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
-public interface PhotoDao {
+public class PhotoDao extends JdbcDaoSupport{
+	
+	@PostConstruct
+	public void initialize(){
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("photomosaic.sql"));
+		DatabasePopulatorUtils.execute(populator, getDataSource());
+		logger.debug("database initialize success!");
+	}
 
-	Photo findByName(String originalFileName);
+	public Photo findByName(String originalFileName) {
+		String sql = "select * from PHOTOS where original_name= ?";
+		RowMapper<Photo> rowMapper = new RowMapper<Photo>() {
+			
+			public Photo mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Photo(
+						rs.getInt("id"),
+						rs.getString("unique_id"),
+						rs.getString("original_name"),
+						rs.getInt("MOSAICS_id"));
+			}
+		};
+		return getJdbcTemplate().queryForObject(sql, rowMapper, originalFileName);
+	}
 
-	void upload(Photo photo);
-
-	void deleteById(Photo photo);
-
-	void deleteAll(Photo photo);
-
-	Photo findByUniqueId(String uniqueId);
-
-	int getNumOfPhotos(int mosaicId);
-
+	public void upload(Photo photo) {
+		String sql="INSERT INTO PHOTOS (unique_id, original_name, width, height, MOSAICS_id) VALUES (?, ?, ?, ?, ?)";
+		getJdbcTemplate().update(sql, photo.getUniqueId(), photo.getOriginalFileName(), photo.getWidth(), photo.getHeight(), photo.getMosaicId());
+	}
+	
+	public void deleteById(Photo photo) {
+		String sql="delete from PHOTOS where id = ? ";
+		getJdbcTemplate().update(sql, photo.getId());
+	}
+	
+	public void deleteAll(Photo photo) {
+		String sql="delete from PHOTOS";
+		getJdbcTemplate().update(sql);
+	}
+	
+	public Photo findByUniqueId(String uniqueId) {
+		String sql = "select * from PHOTOS where unique_id = ?";
+		RowMapper<Photo> rowMapper = new RowMapper<Photo>() {
+			
+			public Photo mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Photo(
+						rs.getInt("id"),
+						rs.getString("unique_id"),
+						rs.getString("original_name"),
+						rs.getInt("MOSAICS_id"));
+			}
+		};
+		return getJdbcTemplate().queryForObject(sql, rowMapper, uniqueId);
+	}
+	
+	public int getNumOfPhotos(int mosaicId) {
+		String sql = "select COUNT(*) from PHOTOS where MOSAICS_id = ?";
+		int numOfPhotos = getJdbcTemplate().queryForObject(sql, new Object[]{mosaicId}, Integer.class);
+		return numOfPhotos;
+	}
 }
