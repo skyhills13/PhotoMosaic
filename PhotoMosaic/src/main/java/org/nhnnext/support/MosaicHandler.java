12c2095699b479676ec400a87544fe1148c6663d
skyhills13 <skyhills13@gmail.com>
2014-11-13 13:39:43 +0900

    fix error

--- PhotoMosaic/src/main/java/org/nhnnext/support/MosaicHandler.java
+++ PhotoMosaic/src/main/java/org/nhnnext/support/MosaicHandler.java
@@ -1,5 +1,6 @@
 package org.nhnnext.support;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,42 +20,50 @@ public class MosaicHandler {
 //	private static final String ATTACHMENT_ROOT_DIR_REMOTE = "";
 //	private static final String ATTACHMENT_ROOT_DIR = "/Users/kimjoohwee/git/PhotoMosaic/PhotoMosaic/webapp/images";
 //	private static final String ATTACHMENT_ROOT_DIR =  "/Users/min/dev/FinalProject/Git Repository/PhotoMosaic/webapp/images";
-	
-	private static final int NUM_OF_PHOTOS = 8;
+
 	private static final int CHUNK_WIDTH = 1000;
 	private static final int CHUNK_HEIGHT = 750;
 	
 	public static void mergePhotos(Mosaic mosaic) throws IOException {
 
-		int rows = 4; 
-		int cols = 4;
+		int rows = 2; // we assume the no. of rows and cols are known and each
+						// chunk has equal width and height
+		int cols = 2;
 		int chunks = rows * cols;
 
+		int chunkWidth, chunkHeight;
 		int type;
 
 		// fetching image files
-		File[] imgFiles = new File[NUM_OF_PHOTOS];
-		BufferedImage[] bufferedImages = new BufferedImage[NUM_OF_PHOTOS];
+		File[] imgFiles = new File[chunks];
 		Photo[] photos = mosaic.getPhotos();
-		
-		for (int i = 0; i < NUM_OF_PHOTOS; i++) {
+		for (int i = 0; i < chunks; i++) {
 			logger.debug("Photo :{}" , photos[i]);
 			logger.debug(ATTACHMENT_ROOT_DIR + File.separator + photos[i].getUniqueId());
 			imgFiles[i] = new File(ATTACHMENT_ROOT_DIR + File.separator + photos[i].getUniqueId());
-			bufferedImages[i] = ImageIO.read(imgFiles[i]);
 		}
 
-		type = bufferedImages[0].getType() == 0 ? BufferedImage.TYPE_INT_ARGB : bufferedImages[0].getType();
+		// creating a bufferd image array from image files
+		BufferedImage[] bufferedImages = new BufferedImage[chunks];
+		for (int i = 0; i < chunks; i++) {
+			bufferedImages[i] = ImageIO.read(imgFiles[i]);
+		}
+		type = bufferedImages[0].getType();
 		
-		
-		
+		logger.debug("type in the mergeImages : " + bufferedImages[0].getType());
+		//criteria of the chunk size is first images size
+		chunkWidth = bufferedImages[0].getWidth();
+		logger.debug("chunkWidth :" + chunkWidth);
+		chunkHeight = bufferedImages[0].getHeight();
+		logger.debug("chunkHeight :" + chunkHeight);
+
 		// Initializing the final image
-		BufferedImage finalImg = new BufferedImage(CHUNK_WIDTH * cols, CHUNK_HEIGHT * rows, type);
+		BufferedImage finalImg = new BufferedImage(chunkWidth * cols, chunkHeight * rows, type);
 
 		int num = 0;
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				finalImg.createGraphics().drawImage(bufferedImages[num], CHUNK_WIDTH * j, CHUNK_HEIGHT * i, null);
+				finalImg.createGraphics().drawImage(bufferedImages[num], chunkWidth * j, chunkHeight * i, null);
 				num++;
 			}
 		}
@@ -72,15 +81,5 @@ public class MosaicHandler {
 	 * 1x3  1x2   0     0
 	 * 0    1x2  2x1    0
 	 * 0     0   2x1    0
-	 */
-	
-//	private BufferedImage cropImage(BufferedImage src, Rectangle rect) {
-//		BufferedImage dest = src.getSubimage(0, 0, rect.width, rect.height);
-//		return dest;
-//	}
-	
-//	private Photo[] randomlyPickPhoto(Photo[] photos){
-//		return pickedPhotos;
-//	}
-	
+	 */	
 }
