5d1fe48f31bf05d7b845ca700e97fa23d202c263
kimjoohwee <kjhwee91@gmail.com>
2014-11-14 14:18:51 +0900

    #149 test용 사진 업로드 A portrait / B 1:1 / C landscape

--- PhotoMosaic/webapp/javascripts/result.js
+++ PhotoMosaic/webapp/javascripts/result.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+window.addEventListener("scroll",function(e){
+	var lightbox = document.querySelector("#lightBox");
+	var isShow = !lightBox.classList.contains("hide");
+	
+	if(isShow){
+		
+	}
+});
