6a16b026b881586d8581dfd045570b250f782ab0
LEE-GyeongMin <min20@nhnnext.org>
2014-12-02 00:06:49 +0900

    #181 변수명 변경

--- PhotoMosaic/webapp/javascripts/lib/TemplateGenerator.js
+++ PhotoMosaic/webapp/javascripts/lib/TemplateGenerator.js
@@ -25,7 +25,7 @@ var Template = (function() {
 			}
 		}
 	};
-	
+
 	
 	/*
 	 * PUBLIC AREA
@@ -48,19 +48,15 @@ var Template = (function() {
 	constructor.prototype.getHeight = function() {
 		return _height;
 	}
-	
-	constructor.prototype.getTemplate = function() {
-		return this.data;
-	};
-	
-	constructor.prototype.setTemplate = function(data) {
+
+	constructor.prototype.setData = function(data) {
 		this.data = data;
 		_countSections.call(this, this.data);
 	};
 	
-	// printTemplate은 data을 예쁘게 반환해준다.
+	// print는 data을 예쁘게 반환해준다. 디버깅 / 개발용
 	// 원래 한 줄 짜리 array라 찍으면 안 예쁘게 나온다.
-	constructor.prototype.printTemplate = function() {
+	constructor.prototype.print = function() {
 		var result = "";
 
 		var idx;
@@ -78,10 +74,6 @@ var Template = (function() {
 		}
 
 		return result;
-	};
-	
-	constructor.prototype.getCounter = function() {
-		return this.counter;
 	};
 
 	constructor.prototype.sumCounter = function() {
@@ -138,8 +130,8 @@ var TemplateGenerator = (function(){
 		//this.createSuitableTemplates(_unitData, 0, 0, sections, 0, 1);
 	};
 	
-	// _canInsert는 template의 특정 좌표에 section을 넣을 수 있는지 검사하는 함수이다.
-	var _canInsert = function(template, section, startX, startY) {
+	// _canInsert는 data의 특정 좌표에 section을 넣을 수 있는지 검사하는 함수이다.
+	var _canInsert = function(data, section, startX, startY) {
 		if (_width - section.x < startX
 				|| _height - section.y < startY) {
 			return false;
@@ -147,7 +139,7 @@ var TemplateGenerator = (function(){
 
 		for (var row = 0; row < section.y; row++) {
 			for (var col = 0; col < section.x; col++) {
-				if (template[(startY + row) * _width + startX + col] !== 0) {
+				if (data[(startY + row) * _width + startX + col] !== 0) {
 					return false;
 				}
 			}
@@ -158,9 +150,9 @@ var TemplateGenerator = (function(){
 
 	// _insert는 template의 특정 좌표에 section을 저장하는 함수이다.
 	// template을 복사해서 작업하고 반환한다.
-	var _insert = function(template, section, startX, startY) {
+	var _insert = function(data, section, startX, startY) {
 		// copy by value
-		var result = template.concat();
+		var result = data.concat();
 
 		for (var row = 0; row < section.y; row++) {
 			for (var col = 0; col < section.x; col++) {
@@ -177,10 +169,10 @@ var TemplateGenerator = (function(){
 	};
 	
 	// _isComplete는 template이 빈칸 없이 완성되었는지를 판별하는 함수이다.
-	var _isComplete = function(template) {
+	var _isComplete = function(data) {
 		for (var row = 0; row < _height; row++) {
 			for (var col = 0; col < _width; col++) {
-				if (template[row * _width + col] === 0) {
+				if (data[row * _width + col] === 0) {
 					return false;
 				}
 			}
@@ -232,7 +224,7 @@ var TemplateGenerator = (function(){
 	}
 	
 	// savePossibleTemplates는 possibleTemplate에 생성 가능한 template을 모두 저장하는 함수이다.
-	constructor.prototype.createAllTemplates = function(template, startX, startY, count) {
+	constructor.prototype.createAllTemplates = function(data, startX, startY, count) {
 		// End condition
 		if (startY === _height) {
 			// 모든 section을 모든 좌표에 넣어본 상황.
@@ -240,7 +232,7 @@ var TemplateGenerator = (function(){
 			if (count === _targetNum) {
 				// 2. possibleTemplate에 현재 template을 저장한다.
 				var newTemplate = new Template(_width, _height);
-				newTemplate.setTemplate(template);
+				newTemplate.setData(data);
 				this.templates.push(newTemplate);
 			}
 		
@@ -254,7 +246,7 @@ var TemplateGenerator = (function(){
 				var section = _sections[row * _width + col];
 
 				// 1-2. 지금 template에 넣을 수 있는지 검사
-				if (_canInsert(template, section, startX, startY)) {
+				if (_canInsert(data, section, startX, startY)) {
 					// boundary over: 들어갈 수 있어도 목표한 갯수가 넘어가버린 경우
 					if (count + 1 > _targetNum) {
 						return;
@@ -266,8 +258,9 @@ var TemplateGenerator = (function(){
 					}
 
 					// 1-3. 넣는다.
-					var tempTemplate = _insert(template, section, startX, startY);
+					var tempTemplate = _insert(data, section, startX, startY);
 
+					// 1-4. 다음 칸에 대한 재귀.
 					if (startX + section.x < _width) {
 						this.createAllTemplates(tempTemplate, startX + section.x, startY, count + 1);
 					} else {
@@ -278,24 +271,24 @@ var TemplateGenerator = (function(){
 		}
 		
 		// 2. 해당 칸에 정보가 들어가있다면 다음 칸으로 넘어간다.
-		if (template[startY * _width + startX] !== 0) {
+		if (data[startY * _width + startX] !== 0) {
 			if (startX === _width - 1) {
-				this.createAllTemplates(template, 0, startY + 1, count);
+				this.createAllTemplates(data, 0, startY + 1, count);
 			} else {
-				this.createAllTemplates(template, startX + 1, startY, count);
+				this.createAllTemplates(data, startX + 1, startY, count);
 			}
 		}
 		// 정보가 없는 경우에는 종료
-		// -> 현재 template, startX, startY로 다시 한 번 1을 수행한다.
+		// -> 현재 data, startX, startY로 다시 한 번 1을 수행한다.
 	};
 
 	// TODO
-	constructor.prototype.createSuitableTemplates = function(template, startX, startY, sections, sIdx, zoomRatio) {
+	constructor.prototype.createSuitableTemplates = function(data, startX, startY, sections, sIdx, zoomRatio) {
 		// End condition
 		if (startY === _height || sIdx >= _targetNum) {
-			if (_isComplete(template)) {
+			if (_isComplete(data)) {
 				var newTemplate = new Template(_width, _height);
-				newTemplate.setTemplate(template);
+				newTemplate.setData(data);
 				this.templates.push(newTemplate);
 			}
 		
@@ -304,9 +297,9 @@ var TemplateGenerator = (function(){
 
 		// 현재 자리에 zoomRatio를 하나씩 증가시키면서 넣어봄.
 		var section = _zoomSection(sections[sIdx], zoomRatio);
-		if (_canInsert(template, section, startX, startY)) {
-			var inserted = _insert(template, section, startX, startY);
-			this.createSuitableTemplates(template, startX, startY, sections, sIdx, zoomRatio + 1);
+		if (_canInsert(data, section, startX, startY)) {
+			var inserted = _insert(data, section, startX, startY);
+			this.createSuitableTemplates(data, startX, startY, sections, sIdx, zoomRatio + 1);
 
 			if (startX === _width - 1) {
 				this.createSuitableTemplates(inserted, 0, startY + 1, sections, sIdx + 1, 1);
@@ -315,21 +308,21 @@ var TemplateGenerator = (function(){
 			}
 		} else {
 			if (startX === _width - 1) {
-				this.createSuitableTemplates(template, 0, startY + 1, sections, sIdx, 1);
+				this.createSuitableTemplates(data, 0, startY + 1, sections, sIdx, 1);
 			} else {
-				this.createSuitableTemplates(template, startX + 1, startY, sections, sIdx, 1);
+				this.createSuitableTemplates(data, startX + 1, startY, sections, sIdx, 1);
 			}
 		}
 
-		if (template[startY * _width + startX] !== 0) {
+		if (data[startY * _width + startX] !== 0) {
 			if (startX === _width - 1) {
-				this.createSuitableTemplates(template, 0, ++startY, sections, sIdx, 1);
+				this.createSuitableTemplates(data, 0, ++startY, sections, sIdx, 1);
 			} else {
-				this.createSuitableTemplates(template, ++startX, startY, sections, sIdx, 1);
+				this.createSuitableTemplates(data, ++startX, startY, sections, sIdx, 1);
 			}
 		}
 
-		console.log(template + ", " + section + ", " + startX + ", " + startY);
+		console.log(data + ", " + section + ", " + startX + ", " + startY);
 	};
 
 	return constructor;
