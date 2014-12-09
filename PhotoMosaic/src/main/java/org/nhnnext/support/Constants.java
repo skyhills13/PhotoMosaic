841be864a244a4a80b64ee2cf8ff1b51613aff80
skyhills13 <skyhills13@gmail.com>
2014-12-09 22:27:18 +0900

    hot fix

--- PhotoMosaic/src/main/java/org/nhnnext/support/Constants.java
+++ PhotoMosaic/src/main/java/org/nhnnext/support/Constants.java
@@ -1,7 +1,7 @@
 package org.nhnnext.support;
 
+
 public class Constants {
-	
 	public static final String UPLOAD_FAIL_MESSAGE = "fail to upload the photo :";
 	public static final String UPLOAD_SUCCESS_MESSAGE = "successfully upload the photo : ";
 	public static final String WRONG_FILE = "No extension for file : ";
@@ -11,9 +11,10 @@ public class Constants {
 	public static final String ALREADY_EXISTING_MEMBER = "이미 존재하는 사용자입니다";
 	public static final String WRONG_PASSWORD = "비밀번호가 틀립니다.";
 	
-//	public static final String ATTACHMENT_ROOT_DIR = "/Users/soeunpark/Documents/workspace/sts/PhotoMosaic/PhotoMosaic/webapp/images";
-//	public static final String ATTACHMENT_ROOT_DIR_REMOTE = "";
-	public static final String ATTACHMENT_ROOT_DIR = "/Users/kimjoohwee/git/PhotoMosaic/PhotoMosaic/webapp/images";
+	public static final String ATTACHMENT_ROOT_DIR = "/Users/soeunpark/Documents/workspace/sts/PhotoMosaic/PhotoMosaic/webapp/images";
+//	public static final String ATTACHMENT_ROOT_DIR = "/home/next/tomcat/webapps/images";
+//	public static final String ATTACHMENT_ROOT_DIR = "/Users/kimjoohwee/git/PhotoMosaic/PhotoMosaic/webapp/images";
 //	public static final String ATTACHMENT_ROOT_DIR =  "/Users/min/dev/FinalProject/Git Repository/PhotoMosaic/webapp/images";
+	public static final String MOSAIC_FILE_EXTENSION = "png";
 	
 }
