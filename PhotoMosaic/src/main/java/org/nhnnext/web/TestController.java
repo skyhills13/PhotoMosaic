6cf52cfa746044203d02bb727162b4114b54fcf8
LEE-GyeongMin <min20@nhnnext.org>
2014-12-16 18:25:26 +0900

    @Controller 어노테이션 빠져있었음.

--- PhotoMosaic/src/main/java/org/nhnnext/web/TestController.java
+++ PhotoMosaic/src/main/java/org/nhnnext/web/TestController.java
@@ -2,9 +2,11 @@ package org.nhnnext.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class TestController {
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 	
