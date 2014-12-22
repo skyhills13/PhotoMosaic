9743e1a1c590ae1c4469491fe589df646253293b
skyhills13 <skyhills13@gmail.com>
2014-12-22 16:39:15 +0900

    #212 make filter for background image of result page

---
+++ PhotoMosaic/src/main/java/org/nhnnext/library/GaussianBlurFilter.java
@@ -0,0 +1,22 @@
+package org.nhnnext.library;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+public class GaussianBlurFilter {
+	
+	public void adaptFilter(String fileName, String destFileName) {
+		try {
+			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+			Mat source = Highgui.imread(fileName, Highgui.CV_LOAD_IMAGE_COLOR);
+			Mat destination = new Mat(source.rows(), source.cols(), source.type());
+			Imgproc.GaussianBlur(source, destination, new Size(45, 45), 0);
+			Highgui.imwrite(destFileName, destination);
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
