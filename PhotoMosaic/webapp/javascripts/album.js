51f14fafa74f91419085269584a736f1c3a49bda
nigayo <niganiga@naver.com>
2014-12-23 11:28:50 +0900

    Update album.js
    코드리뷰.

--- PhotoMosaic/webapp/javascripts/album.js
+++ PhotoMosaic/webapp/javascripts/album.js
@@ -1,9 +1,10 @@
 (function() {
-	var eleBody = document.querySelector("body");
+	var eleBody = document.querySelector("body"); //ele 라고 하지 않고 보통 elBody라고 많이 함. 
 	
 	var eleHeader = document.querySelector("header");
 	var eleAlbums = document.querySelector(".albums");
 	
+	//비슷한 리뷰인데.. 비슷한 이벤트 핸들러를 통합하고 event type으로 구분할 수도 있다는 거.기억바람. 
 	eleHeader.addEventListener("mouseenter", function(event) {
 		var prefix = getBrowserPrefix();
 		var strTransform = "";
