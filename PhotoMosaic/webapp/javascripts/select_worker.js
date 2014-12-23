b121809f6e5a58b61b21acd9a1d091f5c7d5b36e
nigayo <niganiga@naver.com>
2014-12-23 11:30:51 +0900

    Update select_worker.js
    코드리뷰.

--- PhotoMosaic/webapp/javascripts/select_worker.js
+++ PhotoMosaic/webapp/javascripts/select_worker.js
@@ -4,7 +4,7 @@
  */
 self.addEventListener("message", function(event) {
 	var result = {};
-	if (typeof event.data !== "object") {
+	if (typeof event.data !== "object") {  //typeof 는 array도 object 라고 나오고..생각보다 많은 부분이 object라고 나옴. 정확한 타입체크는 toString.call()을 많이 사용. 
 		result["error"] = -1;
 		postMessage(result);
 		return ;
@@ -17,7 +17,7 @@ self.addEventListener("message", function(event) {
 	}
 });
 
-self.functions = {};
+self.functions = {}; //인간적으로 functions과 같은 이름은 예약어 같으니 쓰지말자.
 
 self.functions.toString = function toString(target) {
 	return target.toString();
