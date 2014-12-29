08fe1a45aba48561538b29c9d31c83fe9a69caf6
skyhills13 <skyhills13@gmail.com>
2014-12-30 01:40:48 +0900

    delete obsolete code

--- PhotoMosaic/src/main/java/org/nhnnext/domains/support/DataURL.java
+++ PhotoMosaic/src/main/java/org/nhnnext/domains/support/DataURL.java
@@ -21,17 +21,11 @@ public class DataURL {
 	public String getFileName() {
 		return fileName;
 	}
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
 	
 	public String getDataURL() {
 		return this.dataURL;
 	}
-	public void setDataURL(String dataURL) {
-		this.dataURL = dataURL;
-	}
-	
+
 	@Override
 	public String toString() {
 		return "DataUrl [fileName=" + fileName + ", dataURL=" + dataURL.substring(0,80)+ "..." + "]";
